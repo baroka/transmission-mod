ee9cb34a826fea277618f5e9afc3d7d14ecddf05
baroka <carlos.bares@gmail.com>
2022-11-18 14:04:07 +0100

    commit

--- root/usr/local/bin/magnet.sh
+++ root/usr/local/bin/magnet.sh
@@ -8,7 +8,7 @@ watchDir="/watch"
 inotifywait -m -e create,moved_to $watchDir --includei "\.magnet$" --format %w%f | while read line; do
   echo "add magnet file.. ${line}"
   # Add file to transmission
-  local magnet=`head -1 "$line"`
+  magnet=`head -1 "$line"`
   transmission-remote --add "$magnet"  	    	
   # Remove file
   rm "${line}"
